95b9f06b4ab5a3fe8dbcb493958c8c04f0d6e0ef
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-07-14 13:31:53 +0100

    update environment flags

--- test_fcp.py
+++ test_fcp.py
@@ -4,9 +4,9 @@ import os
 os.environ['XLA_FLAGS'] = (
     '--xla_gpu_enable_triton_softmax_fusion=true '
     '--xla_gpu_triton_gemm_any=True '
-    '--xla_gpu_enable_async_collectives=true '
-    '--xla_gpu_enable_latency_hiding_scheduler=true '
-    '--xla_gpu_enable_highest_priority_async_stream=true '
+    # '--xla_gpu_enable_async_collectives=true '
+    # '--xla_gpu_enable_latency_hiding_scheduler=true '
+    # '--xla_gpu_enable_highest_priority_async_stream=true '
 )
 
 import sys
