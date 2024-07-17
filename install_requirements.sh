532643102c366431fd644bc0414eba11bb57e492
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-07-17 13:13:53 +0100

    add dependencies for memory profiling

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,1 +1,3 @@
 pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html jaxmarl matplotlib pettingzoo imageio orbax-checkpoint
+conda install conda-forge::go conda-forge::graphviz
+go install github.com/google/pprof@latest
