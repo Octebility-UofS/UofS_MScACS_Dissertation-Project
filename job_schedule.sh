d36620cbad740e3f73cf8bd7660c58df35e36ae1
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-08-04 03:15:41 +0100

    increase schedule runtime

--- job_schedule.sh
+++ job_schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu --qos=gpu --gres=gpu:a100:1
 #SBATCH --mem=90G
-#SBATCH --time=08:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=out/log/%j.out
 #SBATCH --mail-type=ALL --mail-user=mlobillich1@sheffield.ac.uk
 
