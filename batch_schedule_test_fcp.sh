6242e3cb9402b329b7806b43ae3c1831a730b908
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-02 16:49:39 +0100

    updated maximum run times

--- batch_schedule_test_fcp.sh
+++ batch_schedule_test_fcp.sh
@@ -2,7 +2,7 @@ sbatch --time=02:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e2 NUM_UPDATES=1e2
 sbatch --time=04:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e3 NUM_UPDATES=1e2 NUM_CHECKPOINTS=10
 sbatch --time=12:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e3 NUM_UPDATES=1e3 NUM_CHECKPOINTS=10
 sbatch --time=24:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e4 NUM_UPDATES=1e3 NUM_CHECKPOINTS=10
-sbatch --time=48:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e4 NUM_UPDATES=1e4 NUM_CHECKPOINTS=10
+sbatch --time=36:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e4 NUM_UPDATES=1e4 NUM_CHECKPOINTS=10
 sbatch --time=96:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e5 NUM_UPDATES=1e4 NUM_CHECKPOINTS=10
 sbatch --time=96:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e5 NUM_UPDATES=1e3 NUM_CHECKPOINTS=10
 sbatch --time=96:00:00 job_schedule.sh test_fcp.py ENV_STEPS=1e3 NUM_UPDATES=1e5 NUM_CHECKPOINTS=10
