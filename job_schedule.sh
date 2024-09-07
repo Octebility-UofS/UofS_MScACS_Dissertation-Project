72a2bb33336fe6efadb6e002844bc433c2c9f132
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-07 17:06:12 +0100

    remove separate slurm log file

--- job_schedule.sh
+++ job_schedule.sh
@@ -6,11 +6,10 @@
 #SBATCH --mail-type=ALL --mail-user=mlobillich1@sheffield.ac.uk
 
 out_path=$1
-slurm_log="out/${out_path}/${SLURM_JOB_ID}_slurm_log.out"
 script_log="out/${out_path}/${SLURM_JOB_ID}_log.out"
 
-echo "$@" >> $slurm_log 2>&1
-echo "JOB ID: $SLURM_JOB_ID" >> $slurm_log 2>&1
+echo "$@"
+echo "JOB ID: $SLURM_JOB_ID"
 
 shift
 script=$1
@@ -18,14 +17,14 @@ shift
 
 module load Anaconda3/2022.05
 module load CUDA/12.1.1
-nvcc --version >> $slurm_log 2>&1
+nvcc --version
 
-module list >> $slurm_log 2>&1
+module list
 
 source activate env_dissertation_project
-which python >> $slurm_log 2>&1
-python -V >> $slurm_log 2>&1
+which python
+python -V
 
 python $script "OUT_PATH=${out_path}" JOB_ID=$SLURM_JOB_ID "$@" >> $script_log 2>&1
 
-echo "Done." >> $slurm_log 2>&1
+echo "Done."
--- job_schedule_cpu.sh
+++ job_schedule_cpu.sh
@@ -5,11 +5,10 @@
 #SBATCH --mail-type=ALL --mail-user=mlobillich1@sheffield.ac.uk
 
 out_path=$1
-slurm_log="out/${out_path}/${SLURM_JOB_ID}_slurm_log.out"
 script_log="out/${out_path}/${SLURM_JOB_ID}_log.out"
 
-echo "$@" >> $slurm_log 2>&1
-echo "JOB ID: $SLURM_JOB_ID" >> $slurm_log 2>&1
+echo "$@"
+echo "JOB ID: $SLURM_JOB_ID"
 
 shift
 script=$1
@@ -17,12 +16,12 @@ shift
 
 module load Anaconda3/2022.05
 
-module list >> $slurm_log 2>&1
+module list
 
 source activate env_dissertation_project
-which python >> $slurm_log 2>&1
-python -V >> $slurm_log 2>&1
+which python
+python -V
 
 python $script "OUT_PATH=${out_path}" JOB_ID=$SLURM_JOB_ID "$@" >> $script_log 2>&1
 
-echo "Done." >> $slurm_log 2>&1
+echo "Done."
