bd29f7809b49b1dfd11151430c6f717e6e1b6eee
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-09 20:23:17 +0100

    change output folder and increase maximum time limits

--- batch_schedule_test_fcp.sh
+++ batch_schedule_test_fcp.sh
@@ -4,16 +4,16 @@ schedule_test_fcp () {
     local time=$1
     local env_steps=$2
     local update_steps=$3
-    sbatch --time=$time --output="out/fcp/mixed/${env_steps}-${update_steps}/%j_slurm_log.out" job_schedule.sh "fcp/mixed/${env_steps}-${update_steps}" test_fcp.py ENV_STEPS=$env_steps NUM_UPDATES=$update_steps NUM_CHECKPOINTS=10
+    sbatch --time=$time --output="out/fcp/mixed_cpu_no_jit/${env_steps}-${update_steps}/%j_slurm_log.out" job_schedule.sh "fcp/mixed_cpu_no_jit/${env_steps}-${update_steps}" test_fcp.py ENV_STEPS=$env_steps NUM_UPDATES=$update_steps NUM_CHECKPOINTS=10
 }
 
 
-schedule_test_fcp 02:00:00 1e1 1e1
-schedule_test_fcp 02:00:00 1e2 1e1
-schedule_test_fcp 05:00:00 1e2 1e2
-schedule_test_fcp 08:00:00 1e3 1e2
-schedule_test_fcp 18:00:00 1e3 1e3
-schedule_test_fcp 72:00:00 1e4 1e3
+schedule_test_fcp 96:00:00 1e1 1e1
+schedule_test_fcp 96:00:00 1e2 1e1
+schedule_test_fcp 96:00:00 1e2 1e2
+schedule_test_fcp 96:00:00 1e3 1e2
+schedule_test_fcp 96:00:00 1e3 1e3
+schedule_test_fcp 96:00:00 1e4 1e3
 schedule_test_fcp 96:00:00 1e4 1e4
 schedule_test_fcp 96:00:00 1e5 1e4
 schedule_test_fcp 96:00:00 1e5 1e3
