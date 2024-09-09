8986405d9ed1dd070eef20a988b3269c4ddcb2a0
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-09 20:50:29 +0100

    update batch schedule time limits

--- batch_schedule_test_td3.sh
+++ batch_schedule_test_td3.sh
@@ -6,15 +6,15 @@ schedule_test_td3 () {
     sbatch --time=$time --output="out/td3/mixed/${env_steps}-${update_steps}-pf${p_freq}/%j_slurm_log.out" job_schedule.sh "td3/mixed/${env_steps}-${update_steps}-pf${p_freq}" test_td3.py REPLAY_ENV_STEPS=$env_steps NUM_UPDATES=$update_steps POLICY_FREQ=$p_freq
 }
 
-schedule_test_td3 12:00:00 1e1 1e1 2
-schedule_test_td3 12:00:00 1e2 1e1 2
-schedule_test_td3 48:00:00 1e2 1e2 2
-schedule_test_td3 48:00:00 1e2 1e2 4
-schedule_test_td3 48:00:00 1e2 1e2 6
-schedule_test_td3 96:00:00 1e3 1e2 2
-schedule_test_td3 96:00:00 1e3 1e2 4
-schedule_test_td3 96:00:00 1e3 1e2 6
-schedule_test_td3 96:00:00 1e3 1e3 4
+schedule_test_td3 00:30:00 1e1 1e1 2
+schedule_test_td3 00:30:00 1e2 1e1 2
+schedule_test_td3 00:30:00 1e2 1e2 2
+schedule_test_td3 00:30:00 1e2 1e2 4
+schedule_test_td3 00:30:00 1e2 1e2 6
+schedule_test_td3 01:00:00 1e3 1e2 2
+schedule_test_td3 01:00:00 1e3 1e2 4
+schedule_test_td3 01:00:00 1e3 1e2 6
+schedule_test_td3 12:00:00 1e3 1e3 4
 schedule_test_td3 96:00:00 1e4 1e3 4
 schedule_test_td3 96:00:00 1e3 1e5 4
 schedule_test_td3 96:00:00 1e3 1e7 4
