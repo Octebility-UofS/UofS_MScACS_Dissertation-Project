fbdc6cc018dfb9d1c2a9e03e25c3e6c067751e5a
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-10 13:07:38 +0100

    implement rollout visualisation for td3

---
+++ td3/rollout.py
@@ -0,0 +1,74 @@
+import jax
+import jax.numpy as jnp
+from brax.io import html
+
+def batchify(x: dict, agent_list: list[str], num_actors: int):
+    x = jnp.stack([ x[a] for a in agent_list ])
+    return x.reshape((num_actors, -1))
+
+def unbatchify(x: jnp.ndarray, agent_list: list[str], num_envs: int, num_actors: int):
+    x = x.reshape((num_actors, num_envs, -1))
+    return {a: x[i] for i, a in enumerate(agent_list)}
+
+def _make_env_step(env):
+
+    NUM_ENVS = 1
+    NUM_ACTORS = env.num_agents * NUM_ENVS
+
+    def _env_step(runner_state, _):
+        actor_state, env_state, env_obs, rng = runner_state
+
+        obs_batch = batchify(env_obs, env.agents, NUM_ACTORS)
+
+        # Select action
+        rng, _rng = jax.random.split(rng)
+        pi = actor_state.apply_fn(actor_state.params, obs_batch)
+        action = pi.sample(seed=_rng)
+        # log_prob = pi.log_prob(action) # Probably not needed, maybe only for IPPO
+        env_act = unbatchify(action, env.agents, NUM_ENVS, env.num_agents)
+        
+        # Step environment
+        rng, _rng = jax.random.split(rng)
+        rng_step = jax.random.split(_rng, NUM_ENVS)
+        env_obs, env_state, reward, done, info = jax.vmap(env.step)(
+            rng_step, env_state, env_act
+        )
+
+        runner_state = (actor_state, env_state, env_obs, rng)
+        return runner_state, env_state
+
+    return _env_step
+
+def _make_rollout(env, num_steps):
+
+    NUM_ENVS = 1
+
+    def _rollout(rng, actor_state):
+        rng, _rng = jax.random.split(rng)
+        reset_rng = jax.random.split(_rng, NUM_ENVS)
+        obsv, env_state = jax.vmap(env.reset)(reset_rng)
+
+        rng, _rng = jax.random.split(rng)
+        runner_state = (actor_state, env_state, obsv, rng)
+        runner_state, rollout_states = jax.lax.scan(
+            _make_env_step(env),
+            runner_state, None,
+            length=num_steps
+        )
+
+        # print(jax.tree.map(lambda x: x[jnp.newaxis, :], env_state))
+        # print(jax.tree.map(lambda x: x.reshape((num_steps, ) + x.shape[2:]), rollout_states))
+        concat_states = jax.tree.map(
+            lambda x, y: jnp.concatenate([x, y.reshape((num_steps, ) + y.shape[2:])], axis=0),
+            env_state, rollout_states
+        )
+        return concat_states
+    return _rollout
+
+def visualise(env, states, num_steps, out_path):
+    state_history = [
+        jax.tree.map(lambda x: x[i], states.pipeline_state) for i in range(num_steps+1)
+    ]
+    rendered_html = html.render(env.sys, state_history)
+    with open(out_path, 'w') as f:
+        f.write(rendered_html)
---
+++ test_td3_rollout.py
@@ -0,0 +1,35 @@
+import sys
+
+import jax
+import jaxmarl
+import optax
+
+from td3.rollout import _make_rollout, visualise
+from td3.td3 import DefaultActor
+from util.util import pickle_load
+from flax.training.train_state import TrainState
+
+
+def main():
+    num_steps = int(sys.argv[1])
+    actor_params_path = sys.argv[2]
+    out_path = sys.argv[3]
+
+    env = jaxmarl.make("ant_4x2")
+    max_action = env.action_spaces[env.agents[0]].high
+    net_actor = DefaultActor(env.action_space(env.agents[0]).shape[0], max_action)
+    actor_params = pickle_load(actor_params_path)
+    state_actor = TrainState.create(
+        apply_fn=net_actor.apply,
+        params=actor_params,
+        tx=optax.adam(3e-4)
+    )
+
+    rng = jax.random.PRNGKey(0)
+    states = jax.jit(_make_rollout(env, num_steps))(rng, state_actor)
+
+    visualise(env, states, num_steps, out_path)
+
+
+if __name__ == "__main__":
+    main()
