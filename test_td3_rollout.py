ed5feea266df91b0bc8f3d1989284c49be12116c
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-12 15:42:16 +0100

    add logwrapper to environment

--- test_td3_rollout.py
+++ test_td3_rollout.py
@@ -13,6 +13,7 @@ from td3.rollout import make_rollout, visualise
 from td3.td3 import DefaultActor
 from util.util import pickle_load
 from flax.training.train_state import TrainState
+from jaxmarl.wrappers.baselines import LogWrapper
 
 
 def main():
@@ -21,6 +22,7 @@ def main():
     out_path = sys.argv[3]
 
     env = jaxmarl.make("ant_4x2")
+    env = LogWrapper(env, replace_info=True)
 
     config = {
         "NUM_STEPS": num_steps,
