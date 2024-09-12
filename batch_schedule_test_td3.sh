1f064abb0383894949e39535fa992618e261f000
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-12 21:25:27 +0100

    update TD3 batch schedule time limits

--- batch_schedule_test_td3.sh
+++ batch_schedule_test_td3.sh
@@ -10,20 +10,20 @@ schedule_test_td3 () {
     REPLAY_ENV_STEPS=$env_steps NUM_UPDATES=$update_steps POLICY_FREQ=$p_freq NUM_ENVS=$environments BATCH_SIZE=$batch_size
 }
 
-schedule_test_td3 01:00:00 1e1 1e1 2 256 256
-schedule_test_td3 01:00:00 1e2 1e1 2 256 1024
+schedule_test_td3 12:00:00 1e1 1e1 2 256 256
+schedule_test_td3 12:00:00 1e2 1e1 2 256 1024
 
-schedule_test_td3 01:00:00 1e2 1e2 2 256 2048
-schedule_test_td3 01:00:00 1e2 1e2 4 256 2048
-schedule_test_td3 01:00:00 1e2 1e2 6 256 2048
+schedule_test_td3 24:00:00 1e2 1e2 2 256 2048
+schedule_test_td3 24:00:00 1e2 1e2 4 256 2048
+schedule_test_td3 24:00:00 1e2 1e2 6 256 2048
 
-schedule_test_td3 01:00:00 1e3 1e2 2 256 8192
-schedule_test_td3 01:00:00 1e3 1e2 4 256 8192
-schedule_test_td3 01:00:00 1e3 1e2 6 256 8192
+schedule_test_td3 96:00:00 1e3 1e2 2 256 8192
+schedule_test_td3 96:00:00 1e3 1e2 4 256 8192
+schedule_test_td3 96:00:00 1e3 1e2 6 256 8192
 
-schedule_test_td3 03:00:00 1e3 1e3 2 256 8192
-schedule_test_td3 24:00:00 1e3 1e4 2 256 8192
+schedule_test_td3 96:00:00 1e3 1e3 2 256 8192
+schedule_test_td3 96:00:00 1e3 1e4 2 256 8192
 schedule_test_td3 96:00:00 1e3 1e5 2 256 8192
 
-schedule_test_td3 24:00:00 1e4 1e3 2 256 16384
+schedule_test_td3 96:00:00 1e4 1e3 2 256 16384
 schedule_test_td3 96:00:00 1e4 1e4 2 256 16384
