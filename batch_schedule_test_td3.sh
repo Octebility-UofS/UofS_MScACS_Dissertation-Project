698dc5c9f64c2a95b99f9735780cf7514e9c4d4a
Oliver Billich <mlobillich1@sheffield.ac.uk>
2024-09-18 01:49:21 +0100

    update batch schedule timings

--- batch_schedule_test_td3.sh
+++ batch_schedule_test_td3.sh
@@ -10,15 +10,15 @@ schedule_test_td3 () {
     TOTAL_STEPS=$env_steps EXPLORATION_STEPS=$explore_steps NUM_ENVS=$environments BATCH_SIZE=$batch_size POLICY_FREQ=$p_freq
 }
 
-schedule_test_td3 01:00:00 4e1 8e2 8 64 2
-schedule_test_td3 01:00:00 8e1 16e2 8 64 2
-schedule_test_td3 01:00:00 16e1 32e2 8 64 2
-schedule_test_td3 01:00:00 8e1 8e4 16 128 2
-schedule_test_td3 01:00:00 16e1 16e4 32 256 2
-schedule_test_td3 01:00:00 16e3 4e6 128 512 2
+schedule_test_td3 12:00:00 4e1 8e2 8 64 2
+schedule_test_td3 48:00:00 8e1 16e2 8 64 2
+schedule_test_td3 48:00:00 16e1 32e2 8 64 2
+schedule_test_td3 48:00:00 8e1 8e4 16 128 2
+schedule_test_td3 96:00:00 16e1 16e4 32 256 2
+schedule_test_td3 96:00:00 16e3 4e6 128 512 2
 
-schedule_test_td3 06:00:00 16e2 16e6 16 4096 2
-schedule_test_td3 12:00:00 16e2 16e7 16 4096 2
-schedule_test_td3 48:00:00 16e2 32e7 16 4096 2
+schedule_test_td3 96:00:00 16e2 16e6 16 4096 2
+schedule_test_td3 96:00:00 16e2 16e7 16 4096 2
+schedule_test_td3 96:00:00 16e2 32e7 16 4096 2
 schedule_test_td3 96:00:00 16e2 64e7 16 4096 2
 schedule_test_td3 96:00:00 16e2 16e8 16 8192 2
